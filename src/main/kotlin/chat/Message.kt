0f13ed2caa181bf5f31108390246559d644f4b0f
Tashila Pathum <tashila.pathum@gmail.com>
2026-02-01 18:54:53 +0530

    Generate Message id and timestamp from server

--- src/main/kotlin/chat/Message.kt
+++ src/main/kotlin/chat/Message.kt
@@ -9,22 +9,18 @@ import kotlin.time.Instant
 /**
  * Represents a single chat message.
  *
- * @property id A unique identifier for the message, often a timestamp or a server-generated ID.
- * Defaults to the current system time in milliseconds for simplicity.
+ * @property id A unique identifier for the message, a timestamp.
  * @property text The actual content of the chat message.
- * @property timestamp The time when the message was sent or received. Defaults to the
- * time of object creation.
- * @property isFromMe A boolean flag to indicate if the message was sent by the current user (true)
- * or received from someone else (false). This is crucial for UI alignment
- * and styling.
+ * @property timestamp The time when the message was sent or received.
+ * @property isFromMe A boolean flag to indicate if the message was sent by the current user (true), or the AI (false).
  */
 @Serializable
 @OptIn(ExperimentalTime::class)
 data class Message constructor(
-    val id: Long = System.currentTimeMillis(),
+    val id: Long,
     val text: String,
     val isFromMe: Boolean,
     val aiThreadId: String? = null,
     @Serializable(with = InstantSerializer::class)
-    val timestamp: Instant = Clock.System.now()
+    val timestamp: Instant
 )
